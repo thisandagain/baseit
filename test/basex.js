6ee3cebae65eca59493c79d9ac787f45a1ffaf7c
Andrew Sliwinski <thisandagain@gmail.com>
2012-01-31 13:59:23 -0800

    Removed minimum string length (append leading zeros)

--- test/basex.js
+++ test/basex.js
@@ -12,34 +12,86 @@ var suite   = vows.describe('BaseX');
  */
 suite.addBatch({
 
+    'Encode 1296 to Base8': {
+
+        topic: function() {
+            basex.encode(1296, 8, this.callback);
+        },
+
+        'is a string': function (err, obj) {
+            assert.isString(obj);
+        },
+        
+        'is equal to "260"': function (err, obj) {
+            assert.strictEqual(obj, "2420");
+        }
+
+    },
+
+    'Decode 2420 to Base10': {
+
+        topic: function() {
+            basex.decode("2420", 8, this.callback);
+        },
+
+        'is a number': function (err, obj) {
+            assert.isNumber(obj);
+        },
+        
+        'is equal to 1296': function (err, obj) {
+            assert.strictEqual(obj, 1296);
+        }
+
+    },
+
+    //
+
     'Encode 1296 to Base24': {
 
         topic: function() {
             basex.encode(1296, 24, this.callback);
         },
 
-        'is a number': function (err, obj) {
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "000100");
+        'is equal to "260"': function (err, obj) {
+            assert.strictEqual(obj, "260");
         }
 
     },
 
-    'Encode 1296 to Base36': {
+    'Decode 260 to Base10': {
 
         topic: function() {
-            basex.encode(1296, 36, {min:6}, this.callback);
+            basex.decode("260", 24, this.callback);
         },
 
         'is a number': function (err, obj) {
+            assert.isNumber(obj);
+        },
+        
+        'is equal to 1296': function (err, obj) {
+            assert.strictEqual(obj, 1296);
+        }
+
+    },
+
+    //
+
+    'Encode 1296 to Base36': {
+
+        topic: function() {
+            basex.encode(1296, 36, this.callback);
+        },
+
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
         'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "000100");
+            assert.strictEqual(obj, "100");
         }
 
     },
@@ -47,15 +99,15 @@ suite.addBatch({
     'Encode "1296" to Base36': {
 
         topic: function() {
-            basex.encode("1296", 36, {min:6}, this.callback);
+            basex.encode("1296", 36, this.callback);
         },
 
-        'is a number': function (err, obj) {
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
         'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "000100");
+            assert.strictEqual(obj, "100");
         }
 
     },
@@ -63,15 +115,15 @@ suite.addBatch({
     'Encode 2771328 to Base36': {
 
         topic: function() {
-            basex.encode(2771328, 36, {min:6}, this.callback);
+            basex.encode(2771328, 36, this.callback);
         },
 
-        'is a number': function (err, obj) {
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
         'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "01nedc");
+            assert.strictEqual(obj, "1nedc");
         }
 
     },
@@ -79,15 +131,15 @@ suite.addBatch({
     'Encode "2771328" to Base36': {
 
         topic: function() {
-            basex.encode("2771328", 36, {min:6}, this.callback);
+            basex.encode("2771328", 36, this.callback);
         },
 
-        'is a number': function (err, obj) {
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
         'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "01nedc");
+            assert.strictEqual(obj, "1nedc");
         }
 
     },
@@ -95,10 +147,10 @@ suite.addBatch({
     'Encode 2176782335 to Base36': {
 
         topic: function() {
-            basex.encode(2176782335, 36, {min:6}, this.callback);
+            basex.encode(2176782335, 36, this.callback);
         },
 
-        'is a number': function (err, obj) {
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
@@ -111,10 +163,10 @@ suite.addBatch({
     'Encode "2176782335" to Base36': {
 
         topic: function() {
-            basex.encode("2176782335", 36, {min:6}, this.callback);
+            basex.encode("2176782335", 36, this.callback);
         },
 
-        'is a number': function (err, obj) {
+        'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
@@ -163,7 +215,7 @@ suite.addBatch({
     'Encode "z3991z1" to Base36 (Invalid Base10 String)': {
 
         topic: function() {
-            basex.encode("z3991z1", 36, {min:6}, this.callback);
+            basex.encode("z3991z1", 36, this.callback);
         },
         
         'returns error': function (err, obj) {
