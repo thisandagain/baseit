6a6b324a301f87f7c242d303f0117f26871651b5
Andrew Sliwinski <thisandagain@gmail.com>
2012-01-31 16:10:09 -0800

    Updated unit tests

--- test/baseit.js
+++ test/baseit.js
@@ -1,11 +1,11 @@
 var vows    = require('vows'),
     assert  = require('assert'),
-    basex   = require('../lib/basex.js');
+    baseit  = require('../lib/baseit.js');
 
 /**
  * Setup
  */
-var suite   = vows.describe('BaseX');
+var suite   = vows.describe('BaseIt');
 
 /**
  * Test
@@ -15,7 +15,10 @@ suite.addBatch({
     'Encode 1296 to Base8': {
 
         topic: function() {
-            basex.encode(1296, 8, this.callback);
+            baseit.encode({
+                input: 1296, 
+                base: 8,
+            }, this.callback);
         },
 
         'is a string': function (err, obj) {
@@ -31,7 +34,10 @@ suite.addBatch({
     'Decode 2420 to Base10': {
 
         topic: function() {
-            basex.decode("2420", 8, this.callback);
+            baseit.decode({
+                input: "2420", 
+                base: 8, 
+            }, this.callback);
         },
 
         'is a number': function (err, obj) {
@@ -49,15 +55,19 @@ suite.addBatch({
     'Encode 1296 to Base24': {
 
         topic: function() {
-            basex.encode(1296, 24, this.callback);
+            baseit.encode({
+                input: 1296, 
+                base: 24,
+                digits: 6,
+            }, this.callback);
         },
 
         'is a string': function (err, obj) {
             assert.isString(obj);
         },
         
-        'is equal to "260"': function (err, obj) {
-            assert.strictEqual(obj, "260");
+        'is equal to "000260"': function (err, obj) {
+            assert.strictEqual(obj, "000260");
         }
 
     },
@@ -65,7 +75,10 @@ suite.addBatch({
     'Decode 260 to Base10': {
 
         topic: function() {
-            basex.decode("260", 24, this.callback);
+            baseit.decode({
+                input: "000260", 
+                base: 24,
+            }, this.callback);
         },
 
         'is a number': function (err, obj) {
@@ -79,75 +92,14 @@ suite.addBatch({
     },
 
     //
-
-    'Encode 1296 to Base36': {
-
-        topic: function() {
-            basex.encode(1296, 36, this.callback);
-        },
-
-        'is a string': function (err, obj) {
-            assert.isString(obj);
-        },
-        
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "100");
-        }
-
-    },
-    
-    'Encode "1296" to Base36': {
-
-        topic: function() {
-            basex.encode("1296", 36, this.callback);
-        },
-
-        'is a string': function (err, obj) {
-            assert.isString(obj);
-        },
-        
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "100");
-        }
-
-    },
-    
-    'Encode 2771328 to Base36': {
-
-        topic: function() {
-            basex.encode(2771328, 36, this.callback);
-        },
-
-        'is a string': function (err, obj) {
-            assert.isString(obj);
-        },
-        
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "1nedc");
-        }
-
-    },
-    
-    'Encode "2771328" to Base36': {
-
-        topic: function() {
-            basex.encode("2771328", 36, this.callback);
-        },
-
-        'is a string': function (err, obj) {
-            assert.isString(obj);
-        },
-        
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, "1nedc");
-        }
-
-    },
     
     'Encode 2176782335 to Base36': {
 
         topic: function() {
-            basex.encode(2176782335, 36, this.callback);
+            baseit.encode({
+                input: 2176782335,
+                base: 36,
+            }, this.callback);
         },
 
         'is a string': function (err, obj) {
@@ -163,7 +115,10 @@ suite.addBatch({
     'Encode "2176782335" to Base36': {
 
         topic: function() {
-            basex.encode("2176782335", 36, this.callback);
+            baseit.encode({
+                input: "2176782335",
+                base: 36,
+            }, this.callback);
         },
 
         'is a string': function (err, obj) {
@@ -178,44 +133,13 @@ suite.addBatch({
     
     //
     
-    'Decode "01nedc" to Base10': {
-
-        topic: function() {
-            basex.decode("01nedc", 36, this.callback);
-        },
-
-        'is a number': function (err, obj) {
-            assert.isNumber(obj);
-        },
-        
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, 2771328);
-        }
-
-    },
-    
-    'Decode "zzzzzz" to Base10': {
-
-        topic: function() {
-            basex.decode("zzzzzz", 36, this.callback);
-        },
-
-        'is a number': function (err, obj) {
-            assert.isNumber(obj);
-        },
-        
-        'is equal to "100"': function (err, obj) {
-            assert.strictEqual(obj, 2176782335);
-        }
-
-    },
-    
-    //
-    
     'Encode "z3991z1" to Base36 (Invalid Base10 String)': {
 
         topic: function() {
-            basex.encode("z3991z1", 36, this.callback);
+            baseit.encode({
+                input: "z3991z1",
+                base: 36,
+            }, this.callback);
         },
         
         'returns error': function (err, obj) {
